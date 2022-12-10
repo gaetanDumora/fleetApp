5205405ee9b41ce864346a78ee30af2fe91e0db9
gaetanDumora <dumoragaetan@gmail.com>
2022-12-11 00:08:14 +0100

    mock location singleton for tests file

--- backend/__tests__/trackVehicules.spec.ts
+++ backend/__tests__/trackVehicules.spec.ts
@@ -1,35 +1,31 @@
-import { trackingLocationSystem } from "../src/modules/location/locationSingleton";
+import { r2d2 } from "../fixtures/r2d2";
 import { describe, expect, it } from "@jest/globals";
 import { tieHunters } from "../fixtures/starWarsVehicules/tieHunterVehicules";
 import { xWingHunters } from "../fixtures/starWarsVehicules/xWingVehicules";
 
-describe("trackingLocationSystem", () => {
+describe("r2d2", () => {
   it("should regiter hunter location when parked, and it can't park twice", () => {
     // Xwing hits hunter 3, he needs to be fixed
     tieHunters[3].park();
     tieHunters[3].park();
 
-    const parkedLocation = trackingLocationSystem.vehiculesLocation.get(
+    const parkedLocation = r2d2.vehiculesLocation.get(
       tieHunters[3].vehiculeId
     )?.location;
 
-    expect(trackingLocationSystem.vehiculesLocation.size).toEqual(1);
+    expect(r2d2.vehiculesLocation.size).toEqual(1);
     expect(parkedLocation).toBeDefined();
   });
 
   it("should throw an error if a Xwing try to park on location taken by a TieHunter", () => {
     // As coordinates are generated randomly, lets pick an existing parked hunter
-    const tieHunter3 = trackingLocationSystem.vehiculesLocation.get(
-      tieHunters[3].vehiculeId
-    );
+    const tieHunter3 = r2d2.vehiculesLocation.get(tieHunters[3].vehiculeId);
     // Get its parked coordinates
     const tieHunter3Location = tieHunter3?.location ?? [];
     // Assign to a new hunter, the taken loaction
     xWingHunters[1].location = tieHunter3Location;
 
-    expect(() =>
-      trackingLocationSystem.trackParkedVehicule(xWingHunters[1])
-    ).toThrow();
+    expect(() => r2d2.trackParkedVehicule(xWingHunters[1])).toThrow();
   });
 
   it("should return the hunter parked at this location", () => {
@@ -41,8 +37,8 @@ describe("trackingLocationSystem", () => {
     const allianceParking = xWingHunters[4].location;
 
     expect(locationBeforePark.length).toEqual(0);
-    expect(
-      trackingLocationSystem.getVehiculesAt([allianceParking])[0].vehiculeId
-    ).toEqual(xWingHunters[4].vehiculeId);
+    expect(r2d2.getVehiculesAt([allianceParking])[0].vehiculeId).toEqual(
+      xWingHunters[4].vehiculeId
+    );
   });
 });
---
+++ backend/fixtures/r2d2.ts
@@ -0,0 +1,8 @@
+import {
+  Location,
+  trackingLocationSystem,
+} from "../src/modules/location/locationSingleton";
+
+export const r2d2: Location = trackingLocationSystem;
+r2d2.takenLocations.clear();
+r2d2.vehiculesLocation.clear();
