0195dbab1800820337708d28ae9315abeac8d3e4
gaetanDumora <dumoragaetan@gmail.com>
2022-12-10 22:58:52 +0100

    setup fastify server

---
+++ backend/src/server.ts
@@ -0,0 +1,26 @@
+import Fastify from "fastify";
+import fleetRoutes from "./modules/fleet/fleetRoute";
+import locationRoutes from "./modules/location/locationRoutes";
+import vehiculeRoutes from "./modules/vehicule/vehiculeRoutes";
+
+const main = async () => {
+  process.on("unhandledRejection", (err) => {
+    console.error(err);
+    process.exit(1);
+  });
+
+  const server = Fastify({
+    logger: true,
+  });
+
+  for (const route of [fleetRoutes, vehiculeRoutes, locationRoutes]) {
+    await server.register(route);
+  }
+
+  await server.listen({
+    port: 3000,
+    host: "127.0.0.1",
+  });
+};
+
+main();
